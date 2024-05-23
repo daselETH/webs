db110d98de1ce1605e525dd59149b3019555c430
daselETH <jancarloinajenes01@gmail.com>
2024-05-24 00:12:43 +0800

    a
    a

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./index.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Rethink Sans:wght@800&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Medium:wght@400&display=swap"
+    />
+  </head>
+  <body>
+    <div class="home-page">
+      <section class="frame-parent">
+        <header class="instructor-parent">
+          <div class="instructor">
+            <li><a href="i-n-s-t-r-u-c-t-o-r.php ">INSTRUCTOR</a> </li>
+          </div>
+          <div class="frame-group">
+            <div class="frame-wrapper">
+              <div class="rectangle-parent">
+                <div class="frame-child"></div>
+                <div class="home">
+                  <li><a href="">home</a> </li>
+                </div>
+              </div>  
+            </div>
+            <div class="schedules">
+              <li><a href="s-c-h-e-d-u-l-e-s .php">Schdules</a> </li>
+            </div>
+          </div>
+        </header>
+        <div class="frame-container">
+          <div class="frame-div">
+            <div class="frame-wrapper1">
+              <div class="detach-the-delays-parent">
+                <div class="detach-the-delays">detach the delays</div>
+                <div class="get-on">GET ON</div>
+                <h1 class="h1">,</h1>
+              </div>
+            </div>
+            <div class="time-parent">
+              <h1 class="time">TIME</h1>
+              <h1 class="h11">” ”</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="frame-section">
+        <div class="d-illustration-hand-with-wrist-parent">
+          <img
+            class="d-illustration-hand-with-wrist-icon"
+            alt=""
+            src="3dillustrationhandwithwristwatch-372682558removebgpreview-2@2x.png"
+          />
+
+          <img
+            class="vuesaxboldlike-tag-icon"
+            loading="lazy"
+            alt=""
+            src="vuesaxboldliketag@2x.png"
+          />
+
+          <img
+            class="vuesaxboldlike-tag-icon1"
+            alt=""
+            src="vuesaxboldliketag@2x.png"
+          />
+
+          <img
+            class="vuesaxboldlike-tag-icon2"
+            alt=""
+            src="vuesaxboldliketag@2x.png"
+          />
+
+          <img
+            class="vuesaxboldlike-tag-icon3"
+            alt=""
+            src="vuesaxboldliketag@2x.png"
+          />
+
+          <img
+            class="vuesaxboldlike-icon"
+            loading="lazy"
+            alt=""
+            src="vuesaxboldlike@2x.png"
+          />
+
+          <img
+            class="vuesaxboldlike-icon1"
+            alt=""
+            src="vuesaxboldlike@2x.png"
+          />
+
+          <div class="ellipse-parent">
+            <div class="frame-item"></div>
+            <div class="frame-inner"></div>
+          </div>
+          <div class="ellipse-group">
+            <div class="ellipse-div"></div>
+            <div class="frame-child1"></div>
+          </div>
+        </div>
+        <div class="rectangle-group">
+          <div class="rectangle-div"></div>
+          <div class="frame-child2"></div>
+          <img
+            class="rectangle-icon"
+            loading="lazy"
+            alt=""
+            src="rectangle-8.svg"
+          />
+
+          <img
+            class="frame-child3"
+            loading="lazy"
+            alt=""
+            src="rectangle-18.svg"
+          />
+
+          <h1 class="instructor-schedules">INSTRUCTOR SCHEDULES</h1>
+          <img
+            class="calendar-icon-vector-schedule-"
+            alt=""
+            src="calendariconvectorscheduledate600nw1725960022--1-removebgpreview-1@2x.png"
+          />
+
+          <div class="frame-parent1">
+            <div class="frame-parent2">
+              <div class="vector-parent">
+                <img
+                  class="frame-child4"
+                  alt=""
+                  src="rectangle-8.svg"
+                />
+
+                <h2 class="room-number-or-container">
+                  <p class="room-number-or">ROOM NUMBER or section</p>
+                  <p class="schedules1">SCHEDULES</p>
+                </h2>
+              </div>
+              <div class="rectangle-container">
+                <div class="frame-child5"></div>
+                <img
+                  class="pkjmii-black-search-button-cli-icon"
+                  loading="lazy"
+                  alt=""
+                  src="pkjmiiblacksearchbuttoncliparthd-1@2x.png"
+                />
+              </div>
+            </div>
+            <div class="dominate-your-day-with-time-t-wrapper">
+              <div class="dominate-your-day">
+                “Dominate your day with TIME, the ultimate scheduling system
+                from the Institute of Computer Studies, designed for effortless
+                productivity.”
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </body>
+</html>
---
+++ s-c-h-e-d-u-l-e-s .php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="global.css" />
+    <link rel="stylesheet" href="s-c-h-e-d-u-l-e-s .css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Rethink Sans:wght@800&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Medium:wght@400&display=swap"
+    />
+  </head>
+  <body>
+    <div class="schedules2">
+      <div class="rectangle-parent2">
+        <div class="frame-child13"></div>
+        <td > <button><a href ="index.php">HOME</td> </a> </button>    
+        <img
+          class="iconhome1"
+          loading="lazy"
+          alt=""
+          src="iconhome.svg"
+        />
+      </div>
+      <section class="schedules-inner">
+        <div class="frame-parent5">
+          <div class="vector-parent2">
+            <img class="frame-child14" alt="" src="rectangle-22.svg" />
+
+            <div class="vector-parent3">  
+              <img
+                class="frame-child15"
+                alt=""
+                src="rectangle-23.svg"
+              />
+
+              <div class="wrapper-illustration-3d-style-1">
+                <img
+                  class="illustration-3d-style-characte-icon1"
+                  loading="lazy"
+                  alt=""
+                  src="illustration3dstylecharactermanagementdelegationauthorityaigeneration-2016063843removebgpreview-2@2x.png"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="vector-parent4">
+            <img class="frame-child16" alt="" src="rectangle-19.svg" />
+            
+
+            <section>
+              <div class="search-box">
+                  <div class="form-value">
+                      <form action="">
+                          <h1>Room or Section </h1>
+                          <div class="inputbox">
+                              <ion-icon name="search"></ion-icon>
+                              <input type="search" required placeholder=""/>
+                            
+                          </div>
+          
+                          <div class="inputbox">
+                              <h1>Semester</h1>
+                              <ion-icon name="lock-Close-outline"></ion-icon>
+                              <input type="text" required placeholder=""/>
+                          </div>
+                          <button>view</button>
+                      </form>  
+                  </div>
+              </div>
+          </section>
+           </div>
+        </div>
+      </section>
+    </div>
+  </body>
+</html>
